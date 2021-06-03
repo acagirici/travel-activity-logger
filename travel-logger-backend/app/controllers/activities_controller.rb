7add963db509cac7fbf4143fe9311b9943060ddd
acagirici <acagirici23@gmail.com>
2021-06-03 02:16:30 -0400

    added delete functionality and started edit

--- travel-logger-backend/app/controllers/activities_controller.rb
+++ travel-logger-backend/app/controllers/activities_controller.rb
@@ -18,6 +18,22 @@ class ActivitiesController < ApplicationController
         end      
     end
 
+    def update
+        activity = Activity.find(params[:id])
+        activity.update(note_params)
+        if activity.save
+            render json: activity, status: 200
+        else
+            render json: {errors: activity.errors.full_messages}, status: 404
+        end 
+    end
+
+    def destroy
+        activity = Activity.find(params[:id])
+        activity.destroy
+        render json: activity, status: 200
+    end
+
     private
 
     def activity_params
